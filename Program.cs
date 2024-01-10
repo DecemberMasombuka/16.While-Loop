f375546d56813d5417b863fa2862eb45176fd38a
A252422 <A252422@LSBSA-19Y5HW3>
2024-01-10 12:53:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            Console.WriteLine("This is a while loop");
+
+            while (index < 5)
+            {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            int index1 = 0;
+            Console.WriteLine("This is a do-while loop");
+            do
+            {
+                Console.WriteLine(index1);
+                index1++;
+            } while (index1 < 50);
+
+
+            Console.Read();
+        }
+    }
+}
